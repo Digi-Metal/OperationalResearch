db25bf599bc908b6aec09eceab659d4953fd3b7e
Matteo <nisimatteo834@gmail.com>
2017-06-10 10:19:42 +0200

    Nisi

--- LAB3/Trash/init_anello.py
+++ LAB3/Trash/init_anello.py
@@ -3,11 +3,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import calendar
 import time
-import fnss
-import scipy.optimize
 
-N =20
-DELTA = 4
+N = 20
+DELTA = 2
 
 def findMaxCardinality(G):
     max = 0
@@ -82,36 +80,42 @@ def findMaxIndexMatrixAndRemove(matrix,N):
     for j in range(N):
             return (i,index)
 
-def findMaxIndexMatrixOnlyMatch(matrix,N,s_u,d_u):
+def findMaxIndexMatrixOnlyMatch(matrix, N, s_u, d_u):
     max = 0
-    indexes = (0,0)
+    indexes = (0, 0)
     for s in range(N):
         for d in range(N):
-           if s_u[s]== 1 and d_u[d] == 1:
-               if matrix[s][d] > max:
+            if s_u[s] == 1 and d_u[d] == 1:
+                if matrix[s][d] > max:
                     max = matrix[s][d]
-                    indexes = (s,d)
+                    indexes = (s, d)
+    matrix[indexes[0]][indexes[1]] = -1
     return indexes
+
+def updateWeight(N,T_matrix):
+    weight = np.inf * np.ones((N, N))
+    for i in range(N):
+        for j in range(N):
+            weight[i][j] = (len(shortest_path[i][j]) - 1) * T_matrix[i][j]
+    return weight
+
 
 
 
 if __name__=='__main__':
-
+    np.random.seed(5)
     mean_vector_f = []
     for x in range(5):
-
         #costruisco anello
         delta_in = np.ones(N)*DELTA
         delta_out = np.ones(N)*DELTA
-
+        weight = np.inf * np.ones((N, N))
         matchable_s = np.ones(N)
         matchable_d = np.ones(N)
 
-
         G = nx.DiGraph()
 
-        T_matrix = np.zeros((N, N))
-        weight = np.inf*np.ones((N,N))
+        T_matrix = np.zeros((N, N)
         print weight
 
         for u in range(N):
@@ -124,9 +128,10 @@ if __name__=='__main__':
         tsd_tmp = T_matrix.copy()
         flagNodes = np.zeros(N)
         flowNodes = []
+        paths = {}
 
         indexes = findMaxIndexMatrixAndRemove(tsd_tmp,N)
-        G.add_edges_from([indexes],weight=tsd_tmp[indexes[0]][indexes[1]])
+        G.add_edges_from([indexes],weight=T_matrix[indexes[0]][indexes[1]])
         weight[indexes[0]][indexes[1]] = T_matrix[indexes[0]][indexes[1]]
         flagNodes[indexes[0]]=1
         flagNodes[indexes[1]]=1
@@ -142,27 +147,29 @@ if __name__=='__main__':
                     G.add_edges_from([(s, d)],weight=tsd_tmp[s][d])
                     weight[s][d] = T_matrix[s][d]
                     flowNodes.append(d)
-                    tsd_tmp[s, d] = 0
+                    tsd_tmp[s][d] = 0
                     flagNodes[d] = 1
                     s = d
                     break
                 else:
                     tsd_tmp[s, d] = 0
 
-        G.add_edges_from([(s, indexes[0])],weight=tsd_tmp[s][indexes[0]])
-        weight[s,indexes[0]] = T_matrix[s,indexes[0]]
-        tsd_tmp[s,indexes[0]] = 0
+    #chiudo l'anello
+        G.add_edges_from([(s, indexes[0])],weight=T_matrix[s][indexes[0]])
+        weight[s][indexes[0]] = T_matrix[s][indexes[0]]
+        tsd_tmp[s][indexes[0]] = 0
 
 
-        print T_matrix
-        print tsd_tmp
-        print flowNodes
-        print G.edges()
-        print weight
+        #
+        # print T_matrix
+        # print tsd_tmp
+        # print flowNodes
+        # print G.edges()
+        # print weight
 
         #nx.draw_networkx(G, arrows=True, with_labels=True)
 
-        print nx.shortest_path(G)
+        # print nx.shortest_path(G)
 
         shortest_path = nx.shortest_path(G)
 
@@ -171,22 +178,32 @@ if __name__=='__main__':
             for j in range(N):
                 weight[i][j] = (len(shortest_path[i][j])-1)*T_matrix[i][j]
 
-
+        print weight
         #add for each arc on the path the tsd
         for i in range(N):
+            paths[i] = {}
             for j in range(N):
-                if len(nx.shortest_path(G,i,j))>2:
-                    edges = nx.shortest_path(G,i,j)
-                    for k in range(1,len(edges)-1):
-                        G.edge[edges[k-1]][edges[k]]['weight'] += T_matrix[i][j]
+                #if len(nx.shortest_path(G,i,j))>2:
+                #todo nel caso rimetti l if
+                edges = nx.shortest_path(G,i,j,weight='weight')
+                paths[i][j] = edges
+                for k in range(1,len(edges)):
+                    G.edge[edges[k-1]][edges[k]]['weight'] += T_matrix[i][j]
+
+
         attempt = 0
+        print paths
+        weight_tmp = weight.copy()
         enter = calendar.timegm(time.gmtime())
         while (sum(list(G.in_degree(range(N)).values())) < N*DELTA or sum(list(G.out_degree(range(N)).values())) < N*DELTA) and calendar.timegm(time.gmtime())-enter<10:
+            #take the "heavier" logical path and create an edge between s and d
             (s,d) = findMaxIndexMatrixOnlyMatch(weight,N,matchable_s,matchable_d)
             if G.in_degree(d) < DELTA and G.out_degree(s)<DELTA:
                 if not G.has_edge(s,d):
-                    edges = nx.shortest_path(G, s, d)
-                    for k in range(1, len(edges) - 1):
+                    edges = paths[s][d]
+                    path = nx.shortest_path(G,s,d)
+                    for k in range(1, len(edges)):
+                        #removing from all the edges the traffic of the edge
                         G.edge[edges[k - 1]][edges[k]]['weight'] -= T_matrix[s][d]
                     G.add_edges_from([(s,d)],weight=T_matrix[s][d])
                     weight[s][d] = T_matrix[s][d]
@@ -197,12 +214,15 @@ if __name__=='__main__':
                     matchable_s[s] = 0
 
 
+
         print matchable_d
         print matchable_s
      #   nx.draw_networkx(G,with_labels=True)
 
         fmax = 0
         (s_f,d_f) = (0,0)
+
+        print G.edge
 
         for s in G.edge:
             for d in G.edge[s]:
@@ -219,8 +239,8 @@ if __name__=='__main__':
         #     print e,G.in_degree(e),G.out_degree(e)
         print 'Fine',x
 
-
-        #plt.show()
+        nx.draw_networkx(G)
+        plt.show()
 
     
     print np.mean(mean_vector_f)
